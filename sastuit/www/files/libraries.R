6535adf263c10bfc884732adb9a10b0f0a436f00
Anibal Herrera <56457623+anibalhc@users.noreply.github.com>
2020-07-24 07:53:15 -0500

    Update libraries.R

--- sastuit/www/files/libraries.R
+++ sastuit/www/files/libraries.R
@@ -50,4 +50,25 @@ library(qdap)
 library(tableHTML)
 
 
-install.packages(setdiff(c("e1071"), rownames(installed.packages())))
+#install.packages(setdiff(c("e1071"), rownames(installed.packages())))
+
+# Function to Install and Load R Packages
+Install_And_Load <- function(Required_Packages)
+{
+    Remaining_Packages <- Required_Packages[!(Required_Packages %in% installed.packages()[,"Package"])];
+
+    if(length(Remaining_Packages)) 
+    {
+        install.packages(Remaining_Packages);
+    }
+    for(package_name in Required_Packages)
+    {
+        library(package_name,character.only=TRUE,quietly=TRUE);
+    }
+}
+
+# Specify the list of required packages to be installed and load    
+Required_Packages=c("e1071");
+
+# Call the Function
+Install_And_Load(Required_Packages);
