6b22747d756a94697988b90132f833e6103773c4
Anibal Herrera <56457623+anibalhc@users.noreply.github.com>
2020-07-24 07:47:08 -0500

    Update libraries.R

--- sastuit/www/files/libraries.R
+++ sastuit/www/files/libraries.R
@@ -51,7 +51,19 @@ library(tableHTML)
 
 
 #library("e1071")
-if (!require('e1071')) install.packages("e1071",repos = "http://cran.us.r-project.org"); library('e1071')
+using<-function(...) {
+    libs<-unlist(list(...))
+    req<-unlist(lapply(libs,require,character.only=TRUE))
+    need<-libs[req==FALSE]
+    if(length(need)>0){ 
+        install.packages(need)
+        lapply(need,require,character.only=TRUE)
+    }
+}
+
+using("e1071")
+
+library("e1071")
 
 
 
